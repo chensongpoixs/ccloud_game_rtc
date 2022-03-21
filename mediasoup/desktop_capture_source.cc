4f8e241d90f0c0edf90c0bf220961be32f2ffce4
chensong <chensongbj25@gamil.com>
2022-03-21 12:05:04 +0800

    [网络情况带宽差 修改分辨率 自适应 带宽哈 ^_^]

--- mediasoup/desktop_capture_source.cc
+++ mediasoup/desktop_capture_source.cc
@@ -4,7 +4,7 @@
 #include "api/video/video_rotation.h"
 #include "rtc_base/logging.h"
 
- 
+#include "third_party/libyuv/include/libyuv.h"
 void VideoCaptureSource::AddOrUpdateSink(
     rtc::VideoSinkInterface<webrtc::VideoFrame>* sink,
     const rtc::VideoSinkWants& wants) {
@@ -49,17 +49,44 @@ void VideoCaptureSource::OnFrame(const webrtc::VideoFrame& frame) {
 		//RTC_LOG(LS_INFO) << "video adapter input failed !!!";
 		return;
 	}
-
+	
 	if (out_height != frame.height() || out_width != frame.width()) {
 		// Video adapter has requested a down-scale. Allocate a new buffer and
 		// return scaled version.
 		// For simplicity, only scale here without cropping.
+		/*
+		
+		 GPU d%
+			
+
+		*/
+		rtc::scoped_refptr<webrtc::I420Buffer> yuv_scaled_buffer =
+			webrtc::I420Buffer::Create(frame.width(), frame.height());
+		
+		libyuv::ConvertToI420(frame.video_frame_buffer()->ToI420()->DataY(), 0, yuv_scaled_buffer->MutableDataY(),
+			yuv_scaled_buffer->StrideY(), yuv_scaled_buffer->MutableDataU(),
+			yuv_scaled_buffer->StrideU(), yuv_scaled_buffer->MutableDataV(),
+			yuv_scaled_buffer->StrideV(), 0, 0, frame.width(), frame.height(), frame.width(),
+			frame.height(), libyuv::kRotate0, libyuv::FOURCC_ARGB); // GL_BGRA，  FOURCC_BGRA 、、GL_BGR
+
 		rtc::scoped_refptr<webrtc::I420Buffer> scaled_buffer =
 			webrtc::I420Buffer::Create(out_width, out_height);
-		scaled_buffer->ScaleFrom(*frame.video_frame_buffer()->ToI420());
+		
+		scaled_buffer->ScaleFrom(*yuv_scaled_buffer->ToI420());
+
+		rtc::scoped_refptr<webrtc::I420Buffer> argb_scaled_buffer =
+			webrtc::I420Buffer::Create(out_width, out_height);
+
+		libyuv::I420ToARGB(scaled_buffer->DataY(), scaled_buffer->StrideY(), 
+			scaled_buffer->DataU(), scaled_buffer->StrideU(),
+			scaled_buffer->DataV(), scaled_buffer->StrideV(),
+			argb_scaled_buffer->MutableDataY(), out_width * 4, out_width, out_height
+			);
+
+
 		webrtc::VideoFrame::Builder new_frame_builder =
 			webrtc::VideoFrame::Builder()
-			.set_video_frame_buffer(scaled_buffer)
+			.set_video_frame_buffer(argb_scaled_buffer)
 			.set_timestamp_rtp(0)
 			.set_timestamp_ms(rtc::TimeMillis())
 			.set_rotation(webrtc::kVideoRotation_0)
